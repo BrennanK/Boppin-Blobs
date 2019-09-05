661e1577c8fddaa411c1b0866312d6e6f1fd542a
Guilherme Oliveira <guilherme.061@gmail.com>
2019-09-05 16:53:22 -0400

    starting behavior tree

---
+++ Bopping_Blobs/Assets/Guilherme/Scripts/BehaviorTree/Building Blocks/Behavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorTree {
+    public enum EReturnStatus {
+        SUCCESS,
+        FAILURE,
+        RUNNING,
+        SUSPENDED
+    }
+
+    public abstract class Behavior {
+        // Initialize() method should be called once and before any call for the Update() method.
+        public abstract void Initialize();
+        // Update() method should be called once every time the behavior tree is updated, until it signals it has terminated
+        public abstract EReturnStatus Update();
+        // Terminate() is called once after the Update() return a signal that it is no longer running
+        public abstract void Terminate(EReturnStatus _status);
+    }
+}
---
+++ Bopping_Blobs/Assets/Guilherme/Scripts/BehaviorTree/Building Blocks/Composite.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorTree {
+    public abstract class Composite : Behavior {
+        protected List<Behavior> m_childrenBehaviors;
+
+        public override void Initialize() {
+            m_childrenBehaviors = new List<Behavior>();
+        }
+
+        public void AddChildBehavior(Behavior _behavior) {
+            m_childrenBehaviors.Add(_behavior);
+        }
+
+        public void RemoveChildBehavior(Behavior _behavior) {
+            m_childrenBehaviors.Remove(_behavior);
+        }
+
+        public void ClearAllChildren() {
+            m_childrenBehaviors.Clear();
+        }
+    }
+}
