f2aaa822556bf23188ac93f2d4735895b9c8bd95
Guilherme Oliveira <guilherme.061@gmail.com>
2019-09-06 11:17:31 -0400

    adding more basic behavior tree scripts

---
+++ Bopping_Blobs/Assets/Guilherme/Scripts/BehaviorTree/Building Blocks/Composite/Filter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorTree {
+    // A filter is a branch that will not execute its child behavior under specific conditions
+    public abstract class Filter : Sequence {
+        public void AddCondition(Behavior _condition) {
+            m_childrenBehaviors.Insert(0, _condition);
+        }
+
+        public void AddAction(Behavior _action) {
+            base.AddChildBehavior(_action);
+        }
+    }
+}
