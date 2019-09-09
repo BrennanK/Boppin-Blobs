76fe87ae760ce96996067fc21097b7dfb8baba82
Guilherme Oliveira <guilherme.061@gmail.com>
2019-09-09 10:39:34 -0400

    removing the jumping

--- Bopping_Blobs/Assets/Guilherme/Scripts/PlayerController.cs
+++ Bopping_Blobs/Assets/Guilherme/Scripts/PlayerController.cs
@@ -7,32 +7,16 @@ public class PlayerController : MonoBehaviour {
     public float playerSpeed;
     [Range(0, 1)]
     public float groundDamping;
+    public float fallingGravityMultiplier = 3f;
 
-    [Header("Jumping Related")]
-    public float jumpHeight = 1f;
-    public float jumpDistance = 1f;
-    public float goingDownGravityMultiplier = 2f;
-    // private variables related to jumping
-    private float m_initialJumpVelocity;
-    private float m_goingUpGravity;
-    private float m_goingDownGravity;
-    private const float km_jumpPressedRememberTime = 0.15f;
-    private const float km_groundedRememberTime = 0.15f;
-    private float m_jumpRememberTime;
-    private float m_groundedRememberTime;
+    [Header("Attack Related")]
+    public float attackDistance = 1f;
 
     // Cached References
     private CharacterController m_characterControllerReference;
     private DigitalJoystick m_digitalJoystickReference;
     private JoyButton m_joyButtonReference;
     private Vector3 m_movementVector;
-
-    private void Awake() {
-        m_initialJumpVelocity = (2 * jumpHeight * playerSpeed) / jumpDistance;
-        m_goingUpGravity = (-2 * jumpHeight * (playerSpeed * playerSpeed)) / (jumpDistance * jumpDistance);
-        m_goingUpGravity /= -Physics.gravity.y;
-        m_goingDownGravity = m_goingUpGravity * goingDownGravityMultiplier;
-    }
 
     private void Start() {
         m_characterControllerReference = GetComponent<CharacterController>();
@@ -41,32 +25,21 @@ public class PlayerController : MonoBehaviour {
     }
 
     private void Update() {
-        m_jumpRememberTime -= Time.deltaTime;
-        m_groundedRememberTime -= Time.deltaTime;
-
         // Getting Plane Movement
         m_movementVector.x = m_digitalJoystickReference.Horizontal * playerSpeed;
         m_movementVector.x = Mathf.Lerp(m_characterControllerReference.velocity.x, m_movementVector.x, groundDamping);
         m_movementVector.z = m_digitalJoystickReference.Vertical * playerSpeed;
         m_movementVector.z = Mathf.Lerp(m_characterControllerReference.velocity.z, m_movementVector.z, groundDamping);
 
-        if(m_characterControllerReference.isGrounded) {
-            m_groundedRememberTime = km_groundedRememberTime;
+        // Drawing Debug Rays
+        Debug.DrawRay(transform.position, transform.forward * attackDistance, Color.red);
+        Debug.DrawRay(transform.position, new Vector3(m_movementVector.x, 0f, m_movementVector.z).normalized * attackDistance, Color.green);
+        transform.LookAt(transform.position + new Vector3(m_movementVector.x, 0f, m_movementVector.z));
+
+        if (m_characterControllerReference.isGrounded) {
             m_movementVector.y = 0;
         } else {
-            m_movementVector.y += (m_goingDownGravity * Time.deltaTime);
-        }
-
-        if(m_joyButtonReference.pressed) {
-            m_jumpRememberTime = km_jumpPressedRememberTime;
-        }
-
-        // Jumping Here
-        if((m_jumpRememberTime > 0) && (m_groundedRememberTime > 0)) {
-            m_jumpRememberTime = 0;
-            m_groundedRememberTime = 0;
-
-            m_movementVector.y = m_initialJumpVelocity;
+            m_movementVector.y += (Physics.gravity.y * fallingGravityMultiplier * Time.deltaTime);
         }
 
         // Updating the character position
