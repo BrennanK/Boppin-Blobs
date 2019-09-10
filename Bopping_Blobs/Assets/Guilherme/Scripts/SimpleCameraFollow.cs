166853128385858a2e9b18a0e23e5a7ae195b850
Guilherme Oliveira <guilherme.061@gmail.com>
2019-09-10 15:35:27 -0400

    simple camera follow

---
+++ Bopping_Blobs/Assets/Guilherme/Scripts/SimpleCameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCameraFollow : MonoBehaviour  {
+    public Transform toFollow;
+    private float zOffset;
+
+    private void Start() {
+        zOffset = transform.position.z - toFollow.position.z;
+    }
+
+    private void Update() {
+        transform.position = new Vector3(toFollow.position.x, transform.position.y, toFollow.position.z + zOffset);
+    }
+}
