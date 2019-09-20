0a075fa51576aa4b6604aab37e9d12e914980dce
Guilherme Oliveira <guilherme.061@gmail.com>
2019-09-20 15:13:41 -0400

    adding player name in the game UI

---
+++ Bopping_Blobs/Assets/Guilherme/Scripts/PlayerInfoUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfoUI : MonoBehaviour {
+    public GameObject tagTextObject;
+    private RectTransform m_rectTransform;
+
+    private void Start() {
+        m_rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void UpdateInfo(Vector3 _playerPosition) {
+        m_rectTransform.position = Camera.main.WorldToScreenPoint(_playerPosition);
+    }
+
+    public void UpdateInfo(Vector3 _playerPosition, bool _isTag) {
+        m_rectTransform.position = Camera.main.WorldToScreenPoint(_playerPosition);
+        tagTextObject.SetActive(_isTag);
+    }
+}
