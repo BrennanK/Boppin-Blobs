cb9ba74e458ea76368cf2087dabcb91f99c17404
Guilherme Oliveira <guilherme.061@gmail.com>
2019-10-14 16:02:01 -0400

    adding android connection logic

---
+++ Bopping_Blobs/Assets/Scripts/StoreServices/Android/AndroidConnection.cs
@@ -0,0 +1,43 @@
+﻿using StoreServices.Core;
+using System;
+using UnityEngine;
+
+#if UNITY_ANDROID
+
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+namespace StoreServices.Android {
+    public class AndroidConnection : IStoreConnection {
+        public bool IsConnected {
+            get {
+                return PlayGamesPlatform.Instance.IsAuthenticated();
+            }
+        }
+
+        public AndroidConnection() {
+            PlayGamesClientConfiguration configuration = new PlayGamesClientConfiguration.Builder().Build();
+            PlayGamesPlatform.InitializeInstance(configuration);
+            PlayGamesPlatform.DebugLogEnabled = Debug.isDebugBuild;
+            PlayGamesPlatform.Activate();
+        }
+
+        public bool Connect(Action callback = null) {
+            PlayGamesPlatform.Instance.Authenticate((success) => {
+                if(success) {
+                    callback?.Invoke();
+                }
+            });
+
+            return IsConnected;
+        }
+
+        public bool Disconnect(Action callback = null) {
+            PlayGamesPlatform.Instance.SignOut();
+            callback?.Invoke();
+            return !IsConnected;
+        }
+    }
+}
+
+#endif
