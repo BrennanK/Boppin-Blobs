8e647dd32e9d01ada0604a186bee9889604c2558
williamfs <CSheu@student.fullsail.edu>
2019-10-15 10:45:55 -0400

    Weapon bug fix & buttons width rearrange

--- Bopping_Blobs/Assets/William/CustomizeProps.cs
+++ Bopping_Blobs/Assets/William/CustomizeProps.cs
@@ -22,24 +22,9 @@ public class CustomizeProps : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        /*if (CustomizeData.instance)
-        {
-            int hatID = CustomizeData.instance.hatIndex;
-            hatModel = customizationManager.hatModels[hatID];
-
-            int eyeID = CustomizeData.instance.eyeIndex;
-            eyeModel = customizationManager.eyeModels[eyeID];
-
-            int colorID = CustomizeData.instance.colorIndex;
-            this.skinColor = customizationManager.skinColor[colorID];
-        }*/
-
-        Debug.Log("hatIndex is: " + PlayerPrefs.GetInt("hatIndex"));
-        Debug.Log("eyeIndex is: " + PlayerPrefs.GetInt("eyeIndex"));
-        Debug.Log("colorIndex is: " + PlayerPrefs.GetInt("colorIndex"));
         hatModel = customizationManager.hatModels[PlayerPrefs.GetInt("hatIndex")];
         eyeModel = customizationManager.eyeModels[PlayerPrefs.GetInt("eyeIndex")];
-        // weaponModel = customizationManager.weaponModels[PlayerPrefs.GetInt("weaponIndex")];
+        weaponModel = customizationManager.weaponModels[PlayerPrefs.GetInt("weaponIndex")];
         skinColor = customizationManager.skinColor[PlayerPrefs.GetInt("colorIndex")];
 
         Material[] mat = player.GetComponent<MeshRenderer>().materials;
@@ -52,7 +37,6 @@ public class CustomizeProps : MonoBehaviour
         customizationManager.activeHat.transform.localScale = new Vector3(1, 1, 1);
 
         // Sets the weapon place according to different scenes
-        /*
         if (SceneManager.GetActiveScene().buildIndex == 0)
         {
             customizationManager.activeWeapon = Instantiate(weaponModel, player.transform.parent.parent);
@@ -63,6 +47,5 @@ public class CustomizeProps : MonoBehaviour
             customizationManager.activeWeapon = Instantiate(weaponModel, player.transform.parent.parent);
             customizationManager.activeWeapon.transform.localPosition = Vector3.right;
         }
-        */
     }
 }
